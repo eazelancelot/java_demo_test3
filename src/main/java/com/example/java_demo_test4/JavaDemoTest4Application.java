0e08ded1164f26701ab4f484f90a579bf45d2efc
eazelancelot <eazelancelot@gmail.com>
2023-04-11 11:12:48 +0800

    first commit

---
+++ src/main/java/com/example/java_demo_test4/JavaDemoTest4Application.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoTest4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoTest4Application.class, args);
+	}
+
+}
